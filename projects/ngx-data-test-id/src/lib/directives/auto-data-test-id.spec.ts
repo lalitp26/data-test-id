c58ea83ab796ebec00a6d174ec87f73feab4aebf
Lalit Patil <lalitp26@gmail.com>
2025-12-15 10:28:39 +0530

    feat: created empty data test id folder and directives (#1)
    related files and folder structure

---
+++ projects/ngx-data-test-id/src/lib/directives/auto-data-test-id.spec.ts
@@ -0,0 +1,8 @@
+import { AutoDataTestId } from './auto-data-test-id';
+
+describe('AutoDataTestId', () => {
+  it('should create an instance', () => {
+    const directive = new AutoDataTestId();
+    expect(directive).toBeTruthy();
+  });
+});
---
+++ projects/ngx-data-test-id/src/lib/directives/auto-data-test-id.ts
@@ -0,0 +1,6 @@
+import { Directive } from '@angular/core';
+
+@Directive({
+  selector: '[libAutoDataTestId]',
+})
+export class AutoDataTestId {}
